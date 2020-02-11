60067f732ae1e01574d9ecbae9cf590634b28083
eriksonJAguiar <erjulioaguiar@gmail.com>
2020-02-10 23:25:26 -0300

    insert zqm socket

--- tracking-token/sharedicom.py
+++ tracking-token/sharedicom.py
@@ -21,14 +21,16 @@ import shutil
 import pandas as pd
 import psutil
 import struct
+import zmq
 
-class  Serversharedicom:
 
-    def __init__(self,path,IP,IPBC,PORT):
+class Serversharedicom:
+
+    def __init__(self, path, IP, IPBC, PORT):
         self.path = path
         self.HOST = IP
-        self.IPBC = str(IPBC)             
-        self.PORT = PORT        
+        self.IPBC = str(IPBC)
+        self.PORT = PORT
         self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.tcp.bind((self.HOST, self.PORT))
         self.tcp.listen(5)
@@ -38,23 +40,21 @@ class  Serversharedicom:
         self.times = []
         self.time = 0
 
-    
-    def __isValidProvider(self,hprovider):
-        
+    def __isValidProvider(self, hprovider):
+
         try:
             if (hprovider in self.users):
                 return True
-            result = requests.post('http://%s:3000/api/registerUser'%(self.IPBC), json={'org':'hprovider', 'user': hprovider, 'msp': 'HProviderMSP'})
-        
+            result = requests.post('http://%s:3000/api/registerUser' % (self.IPBC), json={
+                                   'org': 'hprovider', 'user': hprovider, 'msp': 'HProviderMSP'})
+
             if(result.status_code == 200):
                 self.users.append(hprovider)
                 return True
         except:
             return False
-        
-    
-    
-    def __readPathDicom(self,path):
+
+    def __readPathDicom(self, path):
 
         result = list(Path(path).rglob("*.dcm"))
 
@@ -71,97 +71,90 @@ class  Serversharedicom:
 
         return dirs
 
-
-    def __readAllDicom(self,paths,owner,examType):
+    def __readAllDicom(self, paths, owner, examType):
         files = []
         for path_ in paths:
             try:
                 result = list(Path(path_).rglob("*.dcm"))
                 image = pydicom.dcmread(str(result[0]))
-            
+
                 dicomId = image.data_element('SOPInstanceUID').value
-                
-                requests.post('http://%s:3000/api/createDicom'%(self.IPBC),json={'user': owner, 'dicomId':dicomId,'typeExam':examType, 'owner':owner})
+
+                requests.post('http://%s:3000/api/createDicom' % (self.IPBC), json={
+                              'user': owner, 'dicomId': dicomId, 'typeExam': examType, 'owner': owner})
             except:
-                print('%s File not register'%(dicomId))
+                print('%s File not register' % (dicomId))
                 exit(1)
-                
+
         print('Regiter Successful')
 
-
-    def __readDicom(self,paths, amount):
+    def __readDicom(self, paths, amount):
         pathzip = []
         tokens = []
         if (amount > len(paths)):
             amount = len(paths)-1
         for i in range(amount):
-            rd = random.randint(0,len(paths)-1)
+            rd = random.randint(0, len(paths)-1)
             path_ = paths[rd]
 
-            #result = list(Path(path_).rglob("*.dcm"))
-            result = glob.glob(os.path.join(path_,"*.dcm"))
+            # result = list(Path(path_).rglob("*.dcm"))
+            result = glob.glob(os.path.join(path_, "*.dcm"))
             image = pydicom.dcmread(str(result[0]))
 
             dicomId = image.data_element('PatientID').value
             t = str(datetime.datetime.now().timestamp())
             value = str(dicomId)+t
-            sha =  hashlib.sha256()
+            sha = hashlib.sha256()
             sha.update(value.encode('utf-8'))
             token = sha.hexdigest()
-            
-            zipname = '%s.zip'%(token)
-            newpath = os.path.join(self.path,'shared')
+
+            zipname = '%s.zip' % (token)
+            newpath = os.path.join(self.path, 'shared')
             os.makedirs(newpath, exist_ok=True)
 
-            newzip = os.path.join(self.path,'shared-zip')
+            newzip = os.path.join(self.path, 'shared-zip')
             os.makedirs(newzip, exist_ok=True)
-            zf = zipfile.ZipFile(os.path.join(newzip,zipname), "w")
-    
+            zf = zipfile.ZipFile(os.path.join(newzip, zipname), "w")
+
             for res in result:
                 fname = str(res).split('/')
                 fname = fname[len(fname)-1]
                 image = pydicom.dcmread(str(res))
-                new_tag = ((0x08,0x17))
-                image.add_new(new_tag,'CS',token) 
-                image.save_as(os.path.join(newpath,fname))
-                zf.write(os.path.join(newpath,fname), arcname=fname)
-            
-            shutil.rmtree(newpath)
-            pathzip.append(os.path.join(newzip,zipname))
-            tokens.append(token)
-            
-        
-        return pathzip,tokens
+                new_tag = ((0x08, 0x17))
+                image.add_new(new_tag, 'CS', token)
+                image.save_as(os.path.join(newpath, fname))
+                zf.write(os.path.join(newpath, fname), arcname=fname)
 
-    #req.body.tokenDicom, req.body.to, req.body.toOrganization
-    def __server_socket(self,con):
-        amount = pickle.loads(con.recv(1024))
+            shutil.rmtree(newpath)
+            pathzip.append(os.path.join(newzip, zipname))
+            tokens.append(token)
+
+        return pathzip, tokens
+
+    # req.body.tokenDicom, req.body.to, req.body.toOrganization
+    def __server_socket(self, con):
+        amount = pickle.loads(con.recv())
         time.sleep(1)
-        user = str(con.recv(1024).decode('utf8'))
+        user = con.recv().decode('utf8')
         time.sleep(1)
-        org = str(con.recv(1024).decode('utf8'))
+        org = con.recv().decode('utf8')
         time.sleep(1)
         paths = self.__readPathDicom(self.path)
-        sharefiles,tokens = self.__readDicom(paths,amount)
-        for filename, token in zip(sharefiles,tokens):
+        sharefiles, tokens = self.__readDicom(paths, amount)
+        for filename, token in zip(sharefiles, tokens):
             fname = filename.split('/')
             fname = fname[len(fname)-1]
             con.send(fname.encode('utf8'))
-            with open(str(filename),"rb") as f: 
-                fz = 0
+            with open(str(filename), "rb") as f:
                 data = f.read(1024)
                 print('Sending ...')
                 while(data):
-                    print('File ... %i'%(int(fz/sys.getsizeof(f))))
                     con.send(data)
                     data = f.read(1024)
-                    fz += 1024
-                    time.sleep(0.1)
-                    os.system('clear')
-                
+
                 f.close()
 
-            
+
             print('Done!')
             print('Sent File ...')
 
@@ -192,8 +185,11 @@ class  Serversharedicom:
                 while True:
                     print('Server started ...')
                     print('We have accepting connections in %s:%s'%(self.HOST,self.PORT))
-                    con, cliente = self.tcp.accept()
-                    print('Connected by ', cliente)
+                    # con, cliente = self.tcp.accept()
+                    # print('Connected by ', cliente)
+                    context = zmq.Context()
+                    con = context.socket(zmq.REP)
+                    con.bind("tcp://*:5001")
                     start_new_thread(self.__server_socket,(con,)) 
             except KeyboardInterrupt:
                 tcp.close()
@@ -201,7 +197,7 @@ class  Serversharedicom:
         
             
 
-    #Local Path images
+    # Local Path images
     def registerDicom(self,hprovider, examType):
         try:
             if(self.__isValidProvider(hprovider)):
@@ -243,19 +239,20 @@ class Clientsharedicom:
 
         return False
         
-    #req.body.tokenDicom, req.body.to, req.body.toOrganization
+    # req.body.tokenDicom, req.body.to, req.body.toOrganization
     def requestDicom(self,amount,research,org):
         time_file = []
         block_size = []
         if(self.__isValidReseach(research)):
-            tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            tcp.connect((self.HOST, self.PORT))
-            tcp.send(pickle.dumps(amount))
+            context = zmq.Context()
+            socket = context.socket(zmq.REQ)
+            socket.connect("tcp://{0}:{1}".format(self.HOST, self.PORT))
+            socket.send(pickle.dumps(amount))
             time.sleep(1)
-            tcp.send(research.encode('utf8'))
-            time.sleep(1)
-            tcp.send(org.encode('utf8'))
-            fname = str(tcp.recv(1024).decode('utf8'))
+            socket.send(research.encode('utf8'))
+            socket.sleep(1)
+            socket.send(org.encode('utf8'))
+            fname = str(socket.recv().decode('utf8'))
             while(fname):
                 start_time_file = time.time()
                 size_block = 0
@@ -265,18 +262,18 @@ class Clientsharedicom:
                     os.mkdir('../SharedDicom')
 
                 f = open(fpath, 'wb+')
-                l = tcp.recv(1024)
+                l = socket.recv()
                 size_block += sys.getsizeof(l)
                 print('Recieve ...')
                 while (l):
                     f.write(l)
-                    l = tcp.recv(1024)
+                    l = socket.recv()
                     size_block += sys.getsizeof(l)
                 f.close()        
                 print('Done ..')
                 time_file.append(time.time()-start_time_file)
                 block_size.append(size_block*0.001)
-                fname = str(tcp.recv(1024).decode('utf8'))
+                fname = str(socket.recv().decode('utf8'))
                 time.sleep(1)
             
             tcp.close()
