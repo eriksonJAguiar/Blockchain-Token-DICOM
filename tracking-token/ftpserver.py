b09017bbeb91694e8a9d5427ad3c7f4c91ca6688
eriksonJAguiar <erjulioaguiar@gmail.com>
2020-02-11 17:02:01 -0300

    ftp library

--- tracking-token/ftpserver.py
+++ tracking-token/ftpserver.py
@@ -1,11 +1,12 @@
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+import os
 
 
 def ftp_server(path):
     authorizer = DummyAuthorizer()
-    authorizer.add_user("user", "12345", os.path.join(path, 'shared-zip'), perm="elradfmw") 
+    authorizer.add_user("user", "12345", os.path.join(path, 'shared-zip/'), perm="elradfmw") 
     authorizer.add_anonymous(os.path.join(path, 'shared-zip/'), perm="elradfmw")
     handler = FTPHandler
     handler.authorizer = authorizer
