2fa5f731eb9ab196ea0b804f60da770df336e4e2
EriksonJAguiar <erjulioaguiar@gmail.com>
2020-03-15 21:41:58 -0300

    change build swarm network

--- google-cloud-network/build-contract.sh
+++ google-cloud-network/build-contract.sh
@@ -16,7 +16,7 @@ ORDERER_CA=${CONFIG_ROOT}/crypto-config/ordererOrganizations/healthcare.com/orde
 
 PEER0_HPROVIDER="docker exec
 -e CORE_PEER_LOCALMSPID=HProviderMSP
--e CORE_PEER_ADDRESS=peer0.hprovider.healthcare.com:7051
+-e CORE_PEER_ADDRESS=peer0_hprovider_healthcare_com:7051
 -e CORE_PEER_MSPCONFIGPATH=${HPROVIDER_MSPCONFIGPATH}
 -e CORE_PEER_TLS_ROOTCERT_FILE=${HPROVIDER_TLS_ROOTCERT_FILE}
 cli
@@ -24,11 +24,25 @@ bash"
 
 echo "Create channel ..."
 
-#docker exec -it -e CORE_PEER_LOCALMSPID=HProviderMSP -e CORE_PEER_ADDRESS=peer0.hprovider.healthcare.com:7051 -e CORE_PEER_MSPCONFIGPATH=${HPROVIDER_MSPCONFIGPATH} -e CORE_PEER_TLS_ROOTCERT_FILE=${HPROVIDER_TLS_ROOTCERT_FILE} -e CORE_PEER_TLS_ENABLED=${CORE_PEER_TLS_ENABLED} -e ORDERER_CA=${ORDERER_CA} cli peer channel create -o orderer.healthcare.com:7050 -c ${CHANNEL_NAME} -f ./channel-artifacts/channel.tx --tls --cafile $ORDERER_CA
+docker exec -it -e CORE_PEER_LOCALMSPID=HProviderMSP -e CORE_PEER_ADDRESS=peer0.hprovider.healthcare.com:7051 -e CORE_PEER_MSPCONFIGPATH=${HPROVIDER_MSPCONFIGPATH} -e CORE_PEER_TLS_ROOTCERT_FILE=${HPROVIDER_TLS_ROOTCERT_FILE} -e CORE_PEER_TLS_ENABLED=${CORE_PEER_TLS_ENABLED} -e ORDERER_CA=${ORDERER_CA} cli peer channel create -o orderer.healthcare.com:7050 -c ${CHANNEL_NAME} -f ./channel-artifacts/channel.tx --tls --cafile $ORDERER_CA
 
-#sleep 2
+sleep 2
 
 #----------- Create Channel -------------
+
+echo "join channel for peer0.hprovider ..."
+
+
+docker exec  -e CORE_PEER_LOCALMSPID=HProviderMSP -e CORE_PEER_ADDRESS=peer0.hprovider.healthcare.com:7051 -e CORE_PEER_MSPCONFIGPATH=${HPROVIDER_MSPCONFIGPATH} -e CORE_PEER_TLS_ROOTCERT_FILE=${HPROVIDER_TLS_ROOTCERT_FILE} -e CORE_PEER_TLS_ENABLED=${CORE_PEER_TLS_ENABLED} -e ORDERER_CA=${ORDERER_CA} cli peer channel join -b ${CHANNEL_NAME}.block
+docker exec -it cli peer channel join -b ${CHANNEL_NAME}.block
+
+sleep 2
+
+#---------- Instatiate chaincode ------------------
+
+echo "Instatiate chaincode peer0.hprovider..."
+
+docker exec  -e CORE_PEER_LOCALMSPID=HProviderMSP -e CORE_PEER_ADDRESS=peer0.hprovider.healthcare.com:7051 -e CORE_PEER_MSPCONFIGPATH=${HPROVIDER_MSPCONFIGPATH} -e CORE_PEER_TLS_ROOTCERT_FILE=${HPROVIDER_TLS_ROOTCERT_FILE} -e CORE_PEER_TLS_ENABLED=${CORE_PEER_TLS_ENABLED} -e ORDERER_CA=${ORDERER_CA} cli peer chaincode instantiate -o orderer.healthcare.com:7050 -C ${CHANNEL_NAME} -l node -n ${CONTRACT_NAME} -v 1.0 -c '{"Args":[]}'
 
 echo "join channel for peer1.hprovider ..."
 
---
+++ google-cloud-network/build-swarm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set +x
+export BYFN_CA1_PRIVATE_KEY=$(cd crypto-config/peerOrganizations/hprovider.healthcare.com/ca && ls *_sk)
+export BYFN_CA2_PRIVATE_KEY=$(cd crypto-config/peerOrganizations/research.healthcare.com/ca && ls *_sk)
+export IMAGE_TAG=1.4
+export CHANNEL_NAME=healthchannel
+set -x
+
+
+docker stack deploy --compose-file docker-compose-ca-pc1.yaml ca-node1
+docker stack deploy --compose-file docker-compose-couch-pc1.yaml couch-node1
+docker stack deploy --compose-file docker-compose-cli-pc1.yaml services-node1
+
+
+
+docker stack deploy --compose-file docker-compose-couch-pc2.yaml couch-node2
+docker stack deploy --compose-file docker-compose-cli-pc2.yaml services-node2
+
+
+docker stack deploy --compose-file docker-compose-ca-pc2.yaml ca-pc2
+
+docker stack deploy --compose-file docker-compose-couch-pc3.yaml couch-node3
+docker stack deploy --compose-file docker-compose-cli-pc3.yaml services-node3
+
+
+docker stack deploy --compose-file docker-compose-couch-pc4.yaml couch-node4
+docker stack deploy --compose-file docker-compose-cli-pc4.yaml services-node4
